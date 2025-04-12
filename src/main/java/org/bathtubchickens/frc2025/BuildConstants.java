fb454dcf74425b0086c8dd24cbde0c5e7390b2ef
10219Ryba <cartersryba@gmail.com>
2025-04-12 17:08:35 -0500

    yes

---
+++ src/main/java/org/bathtubchickens/frc2025/BuildConstants.java
@@ -0,0 +1,17 @@
+package org.bathtubchickens.frc2025;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "OffseasonREEFSCAPE";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = -1;
+  public static final String GIT_SHA = "UNKNOWN";
+  public static final String GIT_DATE = "UNKNOWN";
+  public static final String GIT_BRANCH = "UNKNOWN";
+  public static final String BUILD_DATE = "2025-04-12 16:56:02 CDT";
+  public static final long BUILD_UNIX_TIME = 1744494962925L;
+  public static final int DIRTY = 129;
+
+  private BuildConstants() {}
+}
