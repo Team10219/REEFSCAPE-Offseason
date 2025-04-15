3927720fbacafce2ec8e2fee2585f2f4e6ab87f2
10219Ryba <cartersryba@gmail.com>
2025-04-14 21:40:40 -0500

    template

--- src/main/java/org/bathtubchickens/frc2025/BuildConstants.java
+++ src/main/java/org/bathtubchickens/frc2025/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "OffseasonREEFSCAPE";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = -1;
-  public static final String GIT_SHA = "UNKNOWN";
-  public static final String GIT_DATE = "UNKNOWN";
-  public static final String GIT_BRANCH = "UNKNOWN";
-  public static final String BUILD_DATE = "2025-04-12 16:56:02 CDT";
-  public static final long BUILD_UNIX_TIME = 1744494962925L;
-  public static final int DIRTY = 129;
+  public static final int GIT_REVISION = 1;
+  public static final String GIT_SHA = "fb454dcf74425b0086c8dd24cbde0c5e7390b2ef";
+  public static final String GIT_DATE = "2025-04-12 17:08:35 CDT";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2025-04-14 15:08:15 CDT";
+  public static final long BUILD_UNIX_TIME = 1744661295612L;
+  public static final int DIRTY = 1;
 
   private BuildConstants() {}
 }
